21b84442adb288d46fcea7b60461de6c17af7204
Sid <sidharthraj1995@gmail.com>
2022-10-26 19:31:24 -0500

    Initial Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
+/***************************************************************************
+ * PROJECT TO LEARN ABOUT ARDUINO LIBRARIES AND IMPROVE PROGRAMMING SKILLS *
+ ***************************************************************************/
 #include <Arduino.h>
+
+uint32 count = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.printf("<Poop #%d>\n",count++);
+  delay(250);
 }
