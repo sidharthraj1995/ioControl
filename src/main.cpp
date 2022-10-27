03ba742b8137d8fb7d74c0c82286912842c77413
Sid <sidharthraj1995@gmail.com>
2022-10-27 16:14:27 -0500

    Added Toggle feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
-/***************************************************************************
- * PROJECT TO LEARN ABOUT ARDUINO LIBRARIES AND IMPROVE PROGRAMMING SKILLS *
- ***************************************************************************/
+/**********************************************
+ * * PROJECT TO LEARN ABOUT ARDUINO LIBRARIES *
+ *      * AND IMPROVE PROGRAMMING SKILLS      *
+ **********************************************/
 #include <Arduino.h>
 
-uint32 count = 0;
+#define ONBOARD_LED 2
+
+
+void toggleIO(uint8_t pin);
+
+
+uint32_t count = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(ONBOARD_LED, OUTPUT);
+  digitalWrite(ONBOARD_LED, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.printf("<Poop #%d>\n",count++);
+  Serial.println(".");
+  toggleIO(ONBOARD_LED);
   delay(250);
+}
+
+void toggleIO(uint8_t pin) {
+  digitalWrite(pin, !digitalRead(pin));
 }
